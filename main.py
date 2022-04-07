27a1107bae85ecfa9b531e287a938e289148cc9c
liviaceribelli1 <lceribelli.ieu2020@student.ie.edu>
2022-04-07 15:25:00 +0200

    Homework

---
+++ main.py
@@ -0,0 +1,38 @@
+from matplotlib import pylab as plt
+import pandas as pd
+
+pd.plotting.register_matplotlib_converters()
+
+df1 = pd.read_csv("AAPL.csv")
+print(df1.head())
+df1['Date'] = pd.to_datetime(df1.Date)
+print(df1.head())
+
+df2 = pd.read_csv("Samsung.KS.csv")
+print(df2)
+df2['Date'] = pd.to_datetime(df2.Date)
+
+index2 = []
+for date2 in df2.Date:
+    if df1.index[df1.Date == date2].values.size:
+        index2.append(int(df1.index[df1.Date == date2].values[0]))
+print(index2)
+
+plt.figure("Apple")
+plt.plot(df1["Date"], df1["Close"], 'r-', linewidth=0.6, label="APPL")
+plt.xlabel("Dates")
+plt.legend(loc="upper left")
+
+plt.figure("Samsung")
+plt.plot(df2["Date"], df2["Close"], 'r-', linewidth=0.6, label="Samsung")
+plt.xlabel("Dates")
+plt.legend(loc="upper left")
+
+plt.show()
+
+df1['rebounds'] = pd.series[df2]
+
+df1["E"] = ["AAPL", "Samsung"] * 3
+print(df1)
+df1.plot()
+print(df1.plot())
